17746326d19b607f77f3169c35a5b522210cf4c7
tuanpa06 <phamtuananh08082006@gmail.com>
2024-11-10 10:53:35 +0700

    Init project

---
+++ src/main/java/tuandev/com/initial_service/InitialServiceApplication.java
@@ -0,0 +1,13 @@
+package tuandev.com.initial_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tuandev/com/initial_service/InitialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tuandev.com.initial_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
